72a88867ed0ba0126154ea5e17eb726e6f2ca7af
shu- <m153304w@st.u-gakugei.ac.jp>
2015-07-26 12:43:37 +0900

    plot完了，学習データ生成完了

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import scala.util.Random
 import java.io.PrintWriter
 import perceptron.perceptron
 import breeze.linalg._
+import breeze.plot._
 
 object Main{
   
@@ -11,6 +12,20 @@ object Main{
     val class1_data = DenseMatrix.rand(15,2)
     val class2_data = DenseMatrix.rand(15,2) * -1.0d
     
+    //まず学習データを先にplotする
+    val f = Figure()
+    val p = f.subplot(0)
+    
+    p += plot(class1_data(::,0),class1_data(::,1),'.')
+    p += plot(class2_data(::,0),class2_data(::,1),'.')
+    p.xlabel = "x_1"
+    p.ylabel = "x_2"
+    p.title = "perceptron"
+    
+    
+    
+    
+    
     //それぞれのクラスのラベルを用意
     val label1 = DenseMatrix.ones[Double](15,1)
     val label2 = DenseMatrix.ones[Double](15,1) * -1.0d
@@ -18,6 +33,8 @@ object Main{
     //ラベル付与
     val class1 = DenseMatrix.horzcat(class1_data,label1)
     val class2 = DenseMatrix.horzcat(class2_data,label2)
+    
+    
     
    
     //学習データとして1つにまとめる．
@@ -27,19 +44,23 @@ object Main{
 
     //重みの初期値（適当に決めた)
     var w = DenseVector(-0.2, -0.5, -0.4)
-    println("初期値"+ w)
+    println("重みの初期値  "+ w)
+    
     
     //パーセプトロン
     val perceptron = new perceptron
-    for (i <- 0 to 10){
-      println(i + "週目")
-      for (j <-  0 to 29){
+    for (i <- 1 to 4){
+      println("-----"+ i + "周目 -----")
+      for (j <-  0 to train_data.rows -1 ){
+        
         var x:DenseVector[Double] = DenseVector(train_data(j,0),train_data(j,1),train_data(j,2))
-        var new_w = perceptron.train(w, x)
+        var new_w = perceptron.train(w, x, p,f)
         w := new_w
-        println(w)
+        println("学習後    "+ w)
+        
       }
     }
+    f.saveas("resurt.png")
     
     
   }
