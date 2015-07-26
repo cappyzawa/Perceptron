e241333e5d5d15527cab02cbea2fb70ea39124bd
shu- <m153304w@st.u-gakugei.ac.jp>
2015-07-26 12:41:19 +0900

    plotできた

--- src/main/scala/Perceptron.scala
+++ src/main/scala/Perceptron.scala
@@ -1,36 +1,38 @@
 package perceptron{
   
   import breeze.linalg._
+  import breeze.plot._
   
   class perceptron{
     
-    def train(w:DenseVector[Double], x:DenseVector[Double]):DenseVector[Double] = {
-      println("もってきたx" + x)
+    def train(w:DenseVector[Double], x:DenseVector[Double], p:Plot, f:Figure):DenseVector[Double] = {
       val g = this.makeFunction(w,x)
       println(g)
+
+      val x_2 = this.makeLine(w,x)
+      
       
       
       if(x(2) == 1.0){ //クラス1に属してるならgは0より大きいはず
         if(g <= 0.0){
-          println("class1なのに")
+          println("学習する")
           val new_w = this.updateWights(w,x)
-          println(new_w)
           return new_w
         }
         else{
-          println("class1順当")
+          println("OK")
           return w
         }
       }
       else{
         if(g >= 0.0){
-          println("class2なのに")
+          println("学習する")
           val new_w = this.updateWights(w,x)
-          println(new_w)
           return new_w
         }
         else{
-          println("class2順当")
+          println("OK")
+          p += plot(x_2(::,0),x_2(::,1),'-')
           return w
         }
       }
@@ -41,7 +43,7 @@ package perceptron{
   }
     
     private def updateWights(w:DenseVector[Double], x:DenseVector[Double]):DenseVector[Double] = {
-      println("きた")
+
       //正の定数rhoを決める
       val rho = 0.02
       //ベクトルxの作成 x_0は常に1
@@ -51,6 +53,19 @@ package perceptron{
       
       val new_w = w + rho * label * xvec
       return new_w
+    }
+    
+    private def makeLine(w:DenseVector[Double], x:DenseVector[Double]):DenseMatrix[Double] ={
+      //どうやら点と点を結ぶらしい
+      //x_1 = -1のときと,x_1 = 1のときを結べばいい
+      //ここではx_1 = x(0)であるから
+      val x_2_minus = (w(1)*(-1.0d)-w(0))/w(2)
+      val x_2_minus_mat = DenseMatrix(-1.0,x_2_minus)
+      val x_2_plus = (w(1)-w(0))/w(2)
+      val x_2_plus_mat = DenseMatrix(1.0,x_2_plus)
+      val x_2 = DenseMatrix.vertcat(x_2_minus_mat.t,x_2_plus_mat.t)
+      println(x_2)
+      return x_2
     }
   }
   
