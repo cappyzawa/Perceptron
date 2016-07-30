430ec0168b532599df6c96f0ac462771dd9c65a2
shu- <m153304w@st.u-gakugei.ac.jp>
2016-07-31 03:57:14 +0900

    add break

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,45 +1,42 @@
-import scala.util.Random
-import java.io.PrintWriter
-import perceptron.perceptron
 import breeze.linalg._
 import breeze.plot._
 
-object Main{
+object Main {
 
-  def main(args:Array[String]){
+  def main(args: Array[String]) {
     //学習データつくる
     //クラスずつにつくる
     //第1象限にランダムに15個つくる(class1)
-    val class1_data = DenseMatrix.rand(15,2)
+    val firstQuadrant = DenseMatrix.rand(15, 2)
     //第3象限にランダムに15個つくる(class2)
-    val class2_data = DenseMatrix.rand(15,2) * -1.0d
+    val thirdQuadrant = DenseMatrix.rand(15, 2) * -1.0d
 
     val f = Figure()
-    val p = f.subplot(0)
+    var p = f.subplot(0)
 
     //まず学習データを先にplotする
-    p += plot(class1_data(::,0),class1_data(::,1),'.')
-    p += plot(class2_data(::,0),class2_data(::,1),'.')
+    p += plot(firstQuadrant(::, 0), firstQuadrant(::, 1), '.')
+    p += plot(thirdQuadrant(::, 0), thirdQuadrant(::, 1), '.')
     //x_次元数
     p.xlabel = "x_1"
     p.ylabel = "x_2"
-    p.title = "perceptron"
+    p.title = "Perceptron"
 
 
 
     //それぞれのクラスのラベルを用意
     //15行1列で全部1の行列作成（class1のラベル)
-    val label1 = DenseMatrix.ones[Double](15,1)
+    val class1Label = DenseMatrix.ones[Double](15, 1)
     //15行1列で全部-1の行列作成（class2のラベル)
-    val label2 = DenseMatrix.ones[Double](15,1) * -1.0d
+    val class2Label = DenseMatrix.ones[Double](15, 1) * -1.0d
     //このlabelは識別関数を計算するときに利用したいから1 or -1にした
 
     //ラベル付与
-    val class1 = DenseMatrix.horzcat(class1_data,label1)
-    val class2 = DenseMatrix.horzcat(class2_data,label2)
+    val class1 = DenseMatrix.horzcat(firstQuadrant, class1Label)
+    val class2 = DenseMatrix.horzcat(thirdQuadrant, class2Label)
 
     //学習データとして1つにまとめる．
-    val train_data = DenseMatrix.vertcat(class1,class2)
+    val trainData = DenseMatrix.vertcat(class1, class2)
 
     //-------学習データの生成完了-----------//
 
@@ -50,22 +47,27 @@ object Main{
     var flag = 1
 
     //パーセプトロン
-    val perceptron = new perceptron
-    for(i <- 1 to 10 if flag != 0){
-      println("-----"+ i + "周目 -----")
-      flag = 0 //ここで一回flagを0に戻す
-      //データの行数の分だけ学習データは存在
-      for (j <-  0 to train_data.rows -1 ){
-        //学習データのj行をベクトル化する．
-        var x:DenseVector[Double] = DenseVector(train_data(j,0),train_data(j,1),train_data(j,2))
-        //学習しに行く
-        var new_w = perceptron.train(w, x, p,f)
-        if(w != new_w)  flag = 1 //1度でも誤識別があればflagは1になる．
-        w := new_w
-        f.saveas(i+"周目"+ (j+1) +"回目.png")
+    for (i <- 1 to 10 if flag != 0) {
+      if (flag != 0) {
+        println("-----" + i + "周目 -----")
+        flag = 0 //ここで一回flagを0に戻す
+        //データの行数の分だけ学習データは存在
+        for (j <- 0 until trainData.rows) {
+          //学習データのj行をベクトル化する．
+          val x: DenseVector[Double] = DenseVector(trainData(j, 0), trainData(j, 1), trainData(j, 2))
+          //学習しに行く
+          var new_w = Perceptron.train(w, x, p, f)
+          if (w != new_w) {
+            flag = 1 //1度でも誤識別があればflagは1になる．
+          }
+          w := new_w
+          f.saveas(i + "周目" + (j + 1) + "回目.png")
+        }
+      } else {
+        println("終了")
       }
     }
-    
+
   }
 
 }
--- src/main/scala/Perceptron.scala
+++ src/main/scala/Perceptron.scala
@@ -1,70 +1,69 @@
-package perceptron{
-  
-  import breeze.linalg._
-  import breeze.plot._
-  
-  class perceptron{
-    
-    def train(w:DenseVector[Double], x:DenseVector[Double], p:Plot, f:Figure):DenseVector[Double] = {
-      //まず識別関数にw,xを代入して値を計算
-      val g = this.makeFunction(w,x)
 
-      //決定境界をつくる(描写するために作ったメソッド,少し冗長になってしまったかもしれない．)
-      val line = this.makeLine(w,x)
-      //行列で表現した2点を'-'で結ぶ これが決定境界になる．
-      p += plot(line(::,0),line(::,1),'-')
-      
-      
-      if(x(2) == 1.0){ //クラス1に属してるならgは0より大きいはず
-        if(g <= 0.0){
-          val new_w = this.updateWights(w,x)
-          return new_w
-        }
-        else{
-          return w
-        }
+
+import breeze.linalg._
+import breeze.plot._
+
+object Perceptron {
+
+  def train(w: DenseVector[Double], x: DenseVector[Double], p: Plot, f: Figure): DenseVector[Double] = {
+    //まず識別関数にw,xを代入して値を計算
+    val g = this.makeFunction(w, x)
+
+    //決定境界をつくる(描写するために作ったメソッド,少し冗長になってしまったかもしれない．)
+    val line = this.makeLine(w, x)
+    //行列で表現した2点を'-'で結ぶ これが決定境界になる．
+    p += plot(line(::, 0), line(::, 1), '-')
+
+
+    if (x(2) == 1.0) {
+      //クラス1に属してるならgは0より大きいはず
+      if (g <= 0.0) {
+        val new_w = this.updateWights(w, x)
+        return new_w
       }
-      else{
-        if(g >= 0.0){
-          val new_w = this.updateWights(w,x)
-          return new_w
-        }
-        else{
-          return w
-        }
+      else {
+        return w
       }
     }
-    
-    private def makeFunction(w:DenseVector[Double], x:DenseVector[Double]):Double ={
-      return w(0) + w(1)*x(1) + w(2)*x(2)
+    else {
+      if (g >= 0.0) {
+        val new_w = this.updateWights(w, x)
+        return new_w
+      }
+      else {
+        return w
+      }
+    }
   }
-    
-    //誤識別時の重みの更新
-    private def updateWights(w:DenseVector[Double], x:DenseVector[Double]):DenseVector[Double] = {
 
-      //正の定数rhoを決める
-      val rho = 0.2
-      //ベクトルxの作成 x_0は常に1
-      val xvec = DenseVector(1.0,x(0),x(1))
-      //可読性を高めるため一応labelについて記述
-      val label = x(2)
-      
-      //class1ならlabelは1，class2ならlabelは-1であるからこの式は成り立つ
-      val new_w = w + rho * label * xvec
-      return new_w
-    }
-    
-    private def makeLine(w:DenseVector[Double], x:DenseVector[Double]):DenseMatrix[Double] ={
-      //どうやら点と点を結ぶらしい
-      //x_2 = -1のときと,x_2 = 1のときを結べばいい
-      val x_2_minus = (w(1)*w(1)-w(0))/(-1.0)
-      val x_2_minus_mat = DenseMatrix(x_2_minus,-1.0)
-      val x_2_plus = (w(1)*w(1)-w(0))
-      val x_2_plus_mat = DenseMatrix(x_2_plus, 1.0)
-      val x_2 = DenseMatrix.vertcat(x_2_minus_mat.t,x_2_plus_mat.t)
-      return x_2
-    }
+  private def makeFunction(w: DenseVector[Double], x: DenseVector[Double]): Double = {
+    return w(0) + w(1) * x(1) + w(2) * x(2)
   }
+
+  //誤識別時の重みの更新
+  private def updateWights(w: DenseVector[Double], x: DenseVector[Double]): DenseVector[Double] = {
+
+    //正の定数rhoを決める
+    val rho = 0.2
+    //ベクトルxの作成 x_0は常に1
+    val xvec = DenseVector(1.0, x(0), x(1))
+    //可読性を高めるため一応labelについて記述
+    val label = x(2)
+
+    //class1ならlabelは1，class2ならlabelは-1であるからこの式は成り立つ
+    val new_w = w + rho * label * xvec
+    return new_w
+  }
+
+  private def makeLine(w: DenseVector[Double], x: DenseVector[Double]): DenseMatrix[Double] = {
+    //どうやら点と点を結ぶらしい
+    //x_2 = -1のときと,x_2 = 1のときを結べばいい
+    val x_2_minus = (w(1) * w(1) - w(0)) / (-1.0)
+    val x_2_minus_mat = DenseMatrix(x_2_minus, -1.0)
+    val x_2_plus = (w(1) * w(1) - w(0))
+    val x_2_plus_mat = DenseMatrix(x_2_plus, 1.0)
+    val x_2 = DenseMatrix.vertcat(x_2_minus_mat.t, x_2_plus_mat.t)
+    return x_2
+  }
+}
   
-  
- }
